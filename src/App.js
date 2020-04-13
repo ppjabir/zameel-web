9644658d1d9a5fd729e8046226b71d1ed05a00c9
Jabir PP <jabirpp@Jabirs-MacBook-Air.local>
2020-04-13 23:27:04 +0530

    api url changes to fix in loal

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [siteData, setSiteData] = useState(null);
   useEffect(() => {
     const options = {
-      url: '/api/gethometabdata',
+      url: 'https://portal.zameelapp.com/api/gethometabdata',
       method: 'POST',
       headers: {
         'Accept': 'application/json',
